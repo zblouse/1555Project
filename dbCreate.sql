1431e57b41bf20d991612de1374206a9c7ef7a9f
Jacob Winkler <jwinkler1313@gmail.com>
2017-11-10 15:09:42 -0500

    Messages

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,9 +1,10 @@
 CREATE TABLE profile(
-	userID varchar2(20) PRIMARY KEY,
+	userID varchar2(20),
 	name varchar2(50),
 	password varchar2(50),
 	date_of_birth date,
-	lastlogin timestamp
+	lastlogin timestamp,
+	CONSTRAINT p_pk PRIMARY KEY userID
 );
 
 CREATE TABLE friends(
@@ -11,12 +12,25 @@ CREATE TABLE friends(
 	userID2 varchar2(20),
 	JDate date,
 	message varchar2(200),
-	CONSTRAINT f_pk primary key(userID1, userID2)
+	CONSTRAINT f_pk primary key(userID1, userID2),
+	CONSTRAINT F_FK1 FOREIGN KEY(userID1) REFERENCES profile(userID),
+	CONSTRAINT f_fk2 FOREIGN KEY (userID2) REFERENCES profile(userID)
 );
 
 CREATE TABLE pendingFriends(
 	fromID varchar2(20)
 	toID varchar2(20)
 	message varchar2(200)
+	CONSTRAINT pf_pk PRIMARY KEY(fromID, toID),
+	CONSTRAINT pf_fk1 FOREIGN KEY(fromID) REFERENCES profile(userID),
+	CONSTRAINT pf_fk2 FOREIGN KEY(toID) REFERENCES profile(userID)
 );
 
+CREATE TABLE messages(
+	msgID varchar2(20)
+	fromID varchar2(20)
+	message varchar2(20)
+	toUserID varchar2(20)
+	toGroupID varchar2(20)
+	dateSent date
+);
