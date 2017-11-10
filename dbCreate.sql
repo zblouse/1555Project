b60ca0c5e5e6d6da6f56e674096f17d23698d897
Jacob Winkler <jwinkler1313@gmail.com>
2017-11-10 15:54:02 -0500

    NOT NULL

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,7 +1,7 @@
 CREATE TABLE profile(
 	userID varchar2(20),
-	name varchar2(50),
-	password varchar2(50),
+	name varchar2(50) NOT NULL,
+	password varchar2(50) NOT NULL,
 	date_of_birth date,
 	lastlogin timestamp,
 	CONSTRAINT profile_pk PRIMARY KEY userID
@@ -12,23 +12,24 @@ CREATE TABLE friends(
 	userID2 varchar2(20),
 	JDate date,
 	message varchar2(200),
-	CONSTRAINT friends_pk primary key(userID1, userID2),
+	CONSTRAINT friends_pk PRIMARY KEY(userID1, userID2),
 	CONSTRAINT users_fk1 FOREIGN KEY(userID1) REFERENCES profile(userID),
 	CONSTRAINT users_fk2 FOREIGN KEY (userID2) REFERENCES profile(userID)
 );
 
 CREATE TABLE pendingFriends(
-	fromID varchar2(20)
-	toID varchar2(20)
-	message varchar2(200)
+	fromID varchar2(20),
+	toID varchar2(20),
+	message varchar2(200),
 	CONSTRAINT pendingfrineds_pk PRIMARY KEY(fromID, toID),
 	CONSTRAINT profile_fk1 FOREIGN KEY(fromID) REFERENCES profile(userID),
 	CONSTRAINT profile_fk2 FOREIGN KEY(toID) REFERENCES profile(userID)
 );
 
+--need to declare either to userid or group id to not null
 CREATE TABLE messages(
 	msgID varchar2(20),
-	fromID varchar2(20),
+	fromID varchar2(20) NOT NULL,
 	message varchar2(20),
 	toUserID varchar2(20),
 	toGroupID varchar2(20),
@@ -41,7 +42,7 @@ CREATE TABLE messages(
 
 CREATE TABLE messageRecipient(
 	msgID varchar2(20),
-	userID varchar2(20),
+	userID varchar2(20) NOT NULL,
 	CONSTRAINT messagerecipient_pk PRIMARY KEY(msgID),
 	CONSTRAINT profile_fk FOREIGN KEY(userID) REFERENCES profile(userID)	
 );
@@ -54,7 +55,7 @@ CREATE TABLE groups(
 CREATE TABLE groupMembership(
 	gID varchar2(20),
 	userID varchar2(20),
-	role varchar2(20),
+	role varchar2(20) NOt NULL,
 	CONSTRAINT groupMembership_pk PRIMARY KEY (gID,userID),
 	CONSTRAINT groupMembershipToGroup_fk FOREIGN KEY (gID) REFERENCES groups(gID),
 	CONSTRAINT groupMembershipToProfile_fk FOREIGN KEY (userID) REFERENCES profiles(userID)
